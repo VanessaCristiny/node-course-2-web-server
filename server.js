807ccf4bd40b2da85bb3f1ec4c8b8a90312fc117
vanessa <vanessa.cr.vasconcelos@gmail.com>
2019-01-06 21:00:12 -0200

    Adding Portfolio Page

--- server.js
+++ server.js
@@ -44,7 +44,15 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
-    pageTitle: 'About Page',  });
+    pageTitle: 'About Page'
+   });
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page'
+    message: 'Portfolio Page Here'
+    });
 });
 
 app.get('/bad', (req, res) => {
